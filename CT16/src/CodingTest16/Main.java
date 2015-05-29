10b6f088dc02523794b49ce0f1232bc6c7ccf90c
kdhong_msi <kdhong_msi@kdhong_msi-PC>
2015-05-29 14:25:30 +0900

    11

--- CT16/src/CodingTest16/Main.java
+++ CT16/src/CodingTest16/Main.java
@@ -6,7 +6,6 @@ import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.Comparator;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -15,28 +14,81 @@ public class Main {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		File file = new File("D:\\MyWorks\\WorkSpace\\Git\\CT16\\src\\CodingTest16\\file_20150526.log");
+		File file = new File("D:\\Workspace\\Git\\CodingTest\\CT16\\src\\CodingTest16\\file_20150526.log");
 		FileInputStream fis = null;
-		InputStreamReader isr = null;
+		InputStreamReader isb = null;
 		BufferedReader br = null;
+		
+		try{
+			Map<String, Integer> map = new HashMap<String, Integer>();
+			MVC<String, Integer> mvc = new MVC<String, Integer>(map);
+			TreeMap<String, Integer> tm = new TreeMap<String, Integer>(mvc);
+			fis = new FileInputStream(file);
+			isb = new InputStreamReader(fis, "UTF-8");
+			br = new BufferedReader(isb);
+			String tmp;
+			while((tmp = br.readLine()) != null){
+//				System.out.println(tmp);
+				String tmpArr[] = tmp.split(" ");
+//				System.out.println(tmpArr[1]);
+				String Code = tmpArr[1];
+				int errCount = 1;
+//				System.out.println("Code:"+Code);
+//				System.out.println("errCount:"+errCount);
+				if(map.get(Code) != null)
+					errCount = (int)map.get(Code) + 1;
+				map.put(Code, errCount);
+			}
+			System.out.println(map);
+			tm.putAll(map);
+			System.out.println(tm);
+			
+		}catch(Exception e){
+			System.out.println("Error :"+e.getMessage());
+			
+		}finally{
+			try{
+				fis.close();
+			}catch(Exception e){
+				System.out.println(e.getMessage());
+			}
+			
+			try{
+				isb.close();
+			}catch(Exception e){
+				System.out.println(e.getMessage());
+			}
+			
+			try{
+				br.close();
+			}catch(Exception e){
+				System.out.println(e.getMessage());
+			}
+		}
+		
+		
+		
+//		FileInputStream fis = null;
+//		InputStreamReader isr = null;
+//		BufferedReader br = null;
 
-        HashMap<String,Integer> map = new HashMap<String,Integer>();
+//        HashMap<String,Integer> map = new HashMap<String,Integer>();
         //ValueComparator bvc =  new ValueComparator(map);
-        MVC<String, Integer> mvc =  new MVC<String, Integer>(map);
-        TreeMap<String,Integer> sorted_map = new TreeMap<String,Integer>(mvc);
+//        MVC<String, Integer> mvc =  new MVC<String, Integer>(map);
+//        TreeMap<String,Integer> sorted_map = new TreeMap<String,Integer>(mvc);
 
-        map.put("A",1);
-        map.put("B",1);
-        map.put("C",2);
-        map.put("D",1);
-        map.put("A",2);
-        map.put("D",1);
+//        map.put("A",1);
+//        map.put("B",1);
+//        map.put("C",2);
+//        map.put("D",1);
+//        map.put("A",2);
+//        map.put("D",1);
 
-        System.out.println("unsorted map: "+map);
-
-        sorted_map.putAll(map);
-
-        System.out.println("results: "+sorted_map);
+//        System.out.println("unsorted map: "+map);
+//
+//        sorted_map.putAll(map);
+//
+//        System.out.println("results: "+sorted_map);
         
 //		Map<String, Integer> map = new HashMap<String, Integer>();
 //		map.put("404", 1);
@@ -91,52 +143,23 @@ public class Main {
 //		}
 	}
 	
-	public class HttpResult implements Comparable{
-		private String code;
-		private Integer count;
+	public static class MVC<K,V> implements Comparator<K> {
 		
-		public HttpResult(String code, int count){
-			this.code = code;
-			this.count = count;
+		private Map<K,V> map;
+		
+		public MVC(Map<K,V> map) {
+			this.map = map;
 		}
 		
-		public int compareTo(Object o){
-			return count.compareTo(((HttpResult)o).count);
-		}
-	}
-	
-		
-	public static class ValueComparator implements Comparator<String> {
-		
-		Map<String, Double> base;
-		public ValueComparator(Map<String, Double> base) {
-			this.base = base;
-		}
-		
-		// Note: this comparator imposes orderings that are inconsistent with equals.    
-		public int compare(String a, String b) {
-			if (base.get(a) >= base.get(b)) {
+		public int compare(K o1, K o2) {
+			if ((Integer)map.get(o1) >= (Integer)map.get(o2))
 				return -1;
-			} else {
+			else
 				return 1;
-			} // returning 0 would merge keys
 		}
 	}
+
+
 }
 
 
-final class MVC<K,V> implements Comparator<K> {
-
-    private Map<K,V> map;
-
-    public MVC(Map<K,V> map) {
-        this.map = map;
-    }
-
-    public int compare(K o1, K o2) {
-    	if ((Integer)map.get(o1) >= (Integer)map.get(o2))
-    		return -1;
-    	else
-    		return 1;
-    }
-}
